533f1ab211905ab21c7faa25cf5a57e4cb45844c
Yara-Abdelrahem <yaraomran961@gmail.com>
2024-12-10 18:24:49 +0200

    Add User to passwd and group

--- adduser.sh
+++ adduser.sh
@@ -2,6 +2,20 @@
 
 # this script takes a user's name and password to create a user with sudo privilage
 
+#concatenate user line to send to passwd
+concat_passwd() {
+symbol=":"
+concatenated="$1$symbol$2$symbol$3$symbol$4$symbol$5$symbol$6$symbol$7"
+# Print the result
+echo "$concatenated">>/etc/passwd
+}
+concat_prim_group() {
+#group_name:password:GID:user_list
+symbol=":"
+concatenated="$1$symbol$2$symbol$3$symbol$4"
+# Print the result
+echo "$concatenated">>/etc/group
+}
 
 # function to print user name and password for debugging purposes
 print_usrAndPass() {
@@ -24,5 +38,8 @@ read -sp 'Enter a Password: ' PASS
 # creating a home directory for the new user
 print_status "Creating a Home Directory For New User"
 sudo mkdir /home/${NAME}
-
+INFO="$fullName,$roomNumber,$phoneNumber,$workNumber,$otherInfo"
+HOME_DIR="/home/$NAME"
+concat_passwd "$NAME" "X" "$FIRST_FREE_UID" "$FIRST_FREE_GID" "$INFO" "$HOME_DIR" "$SHELL" 
+concat_prim_group "$NAME" "X" "$FIRST_FREE_GID"
 
