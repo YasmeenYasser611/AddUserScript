ce93c7c986b2b0bd0a046e9e3fd219a18f1206b1
Mariam-Hossam-Saleh <mariam.hossam.keshk@gmail.com>
2024-12-10 15:10:24 +0200

    taking input info from user

--- adduser.sh
+++ adduser.sh
@@ -1,28 +1,82 @@
 #!/bin/bash
 
-# this script takes a user's name and password to create a user with sudo privilage
+# This script takes a user's name and password to create a user with sudo privileges.
 
-
-# function to print user name and password for debugging purposes
+# Function to print user name and password for debugging purposes
 print_usrAndPass() {
-	echo # for new line
-	echo ${NAME}
-	echo ${PASS}
+    echo # for new line
+    echo "Username: ${NAME}"
+    echo "Password: ${PASS}"
 }
 
+# Function to print status messages
 print_status() {
-	echo
-	echo $1
+    echo
+    echo "$1"
 }
 
-# prompting user to enter a name
+# Prompting user to enter a name
 read -p 'Enter a User Name: ' NAME
 
-# prompting user to enter a password (user will not see what they write)
-read -sp 'Enter a Password: ' PASS
+# Prompting user to enter a password (user will not see what they write)
+while true; do
+    read -sp 'Enter user Password: ' PASS
+    echo # Add a new line after password input
+    read -sp 'Re-enter the Password: ' RPASS
+    echo # Add a new line after re-entering password
 
-# creating a home directory for the new user
-print_status "Creating a Home Directory For New User"
-sudo mkdir /home/${NAME}
+    if [ "$PASS" == "$RPASS" ]; then
+        break
+    else
+        echo "Password does not match!"
+        read -p 'Do you want to try again? [yes/no]: ' option
+        if [ "$option" != "yes" ]; then
+            echo "Exiting script."
+            exit 1
+        fi
+    fi
+done
 
+echo 'Adding user information...'
 
+correctInfo="no"
+while [ "$correctInfo" != "yes" ]; do
+    echo 'Enter your values or press Enter for default values'
+    read -p 'User Full Name: ' fullName
+    read -p 'Room Number: ' roomNumber
+    read -p 'Personal Phone Number: ' phoneNumber
+    read -p 'Work Phone Number: ' workNumber
+    read -p 'Other info: ' otherInfo
+
+    echo "Review the entered information:"
+    echo "Full Name: ${fullName}"
+    echo "Room Number: ${roomNumber}"
+    echo "Personal Phone Number: ${phoneNumber}"
+    echo "Work Phone Number: ${workNumber}"
+    echo "Other Info: ${otherInfo}"
+
+    read -p 'Is your info correct? [yes/no]: ' correctInfo
+done
+
+# Find the first free UID
+FIRST_FREE_UID=$(awk -F: -v min_uid=1000 '$3 >= min_uid {uids[$3]=1} END {for (i=min_uid; i<60000; i++) if (!uids[i]) {print i; exit}}' /etc/passwd)
+
+# Find the first free GID
+FIRST_FREE_GID=$(awk -F: -v min_gid=1000 '$3 >= min_gid {gids[$3]=1} END {for (i=min_gid; i<60000; i++) if (!gids[i]) {print i; exit}}' /etc/group)
+
+# Creating a home directory for the new user
+print_status "Creating a Home Directory for New User"
+sudo mkdir -p /home/${NAME}
+
+# Manually adding the user to /etc/passwd
+#print_status "Adding the user to /etc/passwd"
+#sudo bash -c "echo '${NAME}:x:$FIRST_FREE_UID:$FIRST_FREE_GID:${fullName},${roomNumber},${phoneNumber},${workNumber},${otherInfo}:/home/${NAME}:/bin/bash' >> /etc/passwd"
+
+# Manually adding the user to /etc/group
+#sudo bash -c "echo '${NAME}:x:$FIRST_FREE_GID' >> /etc/group"
+
+# Adding the user to the sudoers group
+#print_status "Granting sudo privileges to the user..."
+#sudo bash -c "echo '${NAME} ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers"
+
+#print_status "User ${NAME} created successfully with sudo privileges."
