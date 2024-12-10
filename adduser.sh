17f34cab2438c3305f7fbca126727805100e7942
YasmeenYasser611 <yasmeenyasser611@gmail.com>
2024-12-10 09:41:59 +0200

    creating a home directory for user

--- adduser.sh
+++ adduser.sh
@@ -3,13 +3,26 @@
 # this script takes a user's name and password to create a user with sudo privilage
 
 
+# function to print user name and password for debugging purposes
+print_usrAndPass() {
+	echo # for new line
+	echo ${NAME}
+	echo ${PASS}
+}
+
+print_status() {
+	echo
+	echo $1
+}
+
 # prompting user to enter a name
 read -p 'Enter a User Name: ' NAME
 
 # prompting user to enter a password (user will not see what they write)
 read -sp 'Enter a Password: ' PASS
 
-# echo NAME and PASS to see if they stored the input values
-echo # for new line
-echo ${NAME}
-echo ${PASS}
+# creating a home directory for the new user
+print_status "Creating a Home Directory For New User"
+sudo mkdir /home/${NAME}
+
+
