2f5ad7ad76ba46002f25889a15451d8ae09c2ac0
AhmedAlyElGhannam <ahmedaly.g112@gmail.com>
2024-12-11 09:42:24 +0200

    cleaned up a bit + added if condition for sudo privilages

--- adduser.sh
+++ adduser.sh
@@ -2,39 +2,53 @@
 
 # This script takes a user's name and password to create a user with sudo privileges.
 
-#concatenate user line to send to passwd
+# concatenate user line to send to /etc/passwd file
 concat_passwd() {
-symbol=":"
-concatenated="$1$symbol$2$symbol$3$symbol$4$symbol$5$symbol$6$symbol$7"
-# Print the result
-echo "$concatenated">>/etc/passwd
+	symbol=":"
+	PASSWD_STR="$1$symbol$2$symbol$3$symbol$4$symbol$5$symbol$6$symbol$7"
+
+	# echoing string into passwd
+	print_status "Adding ${NAME} to /etc/passwd..."
+	echo ${PASSWD_STR} >> /etc/passwd
 }
+
+# concatenate group line to send to /etc/group
 concat_prim_group() {
-#group_name:password:GID:user_list
-symbol=":"
-concatenated="$1$symbol$2$symbol$3$symbol$4"
-# Print the result
-echo "$concatenated">>/etc/group
+	symbol=":"
+	GROUP_STR="$1$symbol$2$symbol$3$symbol$4"
+
+	# echoing string into /etc/group
+	print_status "Adding ${NAME}'s Group..."
+	echo ${GROUP_STR} >> /etc/group
+
 }
 
-# function to print user name and password for debugging purposes
-
-print_usrAndPass() {
-    echo # for new line
-    echo "Username: ${NAME}"
-    echo "Password: ${PASS}"
-}
-
-# Function to print status messages
+# function to print status messages
 print_status() {
     echo
     echo "$1"
 }
 
-# Prompting user to enter a name
+
+# force user to run the script as root or with sudo privilages
+if [ ! "`whoami`" = "root" ] then
+	echo 
+    	echo "Please run script as root or using sudo."
+    	exit 1
+fi
+
+
+# finding the first free UID
+FIRST_FREE_UID=$(awk -F: -v min_uid=1000 '$3 >= min_uid {uids[$3]=1} END {for (i=min_uid; i<60000; i++) if (!uids[i]) {print i; exit}}' /etc/passwd)
+
+# finding the first free GID
+FIRST_FREE_GID=$(awk -F: -v min_gid=1000 '$3 >= min_gid {gids[$3]=1} END {for (i=min_gid; i<60000; i++) if (!gids[i]) {print i; exit}}' /etc/group)
+
+
+# prompting user to enter a name
 read -p 'Enter a User Name: ' NAME
 
-# Prompting user to enter a password (user will not see what they write)
+# prompting user to enter a password (user will not see what they write)
 while true; do
     read -sp 'Enter user Password: ' PASS
     echo # Add a new line after password input
@@ -53,7 +67,8 @@ while true; do
     fi
 done
 
-echo 'Adding user information...'
+
+print_status "Adding user information..."
 
 correctInfo="no"
 while [ "$correctInfo" != "yes" ]; do
@@ -74,39 +89,43 @@ while [ "$correctInfo" != "yes" ]; do
     read -p 'Is your info correct? [yes/no]: ' correctInfo
 done
 
-# Find the first free UID
-FIRST_FREE_UID=$(awk -F: -v min_uid=1000 '$3 >= min_uid {uids[$3]=1} END {for (i=min_uid; i<60000; i++) if (!uids[i]) {print i; exit}}' /etc/passwd)
-
-# Find the first free GID
-FIRST_FREE_GID=$(awk -F: -v min_gid=1000 '$3 >= min_gid {gids[$3]=1} END {for (i=min_gid; i<60000; i++) if (!gids[i]) {print i; exit}}' /etc/group)
-
-
 # Manually adding the user to /etc/passwd
-#print_status "Adding the user to /etc/passwd"
-#sudo bash -c "echo '${NAME}:x:$FIRST_FREE_UID:$FIRST_FREE_GID:${fullName},${roomNumber},${phoneNumber},${workNumber},${otherInfo}:/home/${NAME}:/bin/bash' >> /etc/passwd"
+#print_status "User ${NAME} created successfully with sudo privileges."
+
+# creating a home directory for the new user
+print_status "Creating a Home Directory For ${NAME}"
+HOME_DIR="/home/$NAME"
+cd 
+sudo mkdir /home/${NAME}
+
+# Copying configuration files to user's home dir
+print_status "Copying Configurations Files for ${NAME} from /etc/skel"
+sudo cp -r /etc/skel/. /home/${NAME}
+
+# encrypting password
+PASS_ENCRYPTED=$(echo -n "${PASS}" | sha256sum)
+
+# appending extra info into one string
+INFO="$fullName,$roomNumber,$phoneNumber,$workNumber,$otherInfo"
+
+# concatenating all fields required for /etc/passwd
+concat_passwd "$NAME" "x" "$FIRST_FREE_UID" "$FIRST_FREE_GID" "$INFO" "$HOME_DIR" "$SHELL" 
+
+# concatenating all fields required for /etc/group
+concat_prim_group "$NAME" "x" "$FIRST_FREE_GID"
+
+
+# echoing string into passwd
+print_status "Adding ${NAME} to /etc/passwd..."
+echo ${concat_passwd} >> /etc/passwd
+
+# echoing string into /etc/group
+print_status "Adding ${NAME}'s Group..."
+echo ${concat_prim_group} >> /etc/group
 
 
 # Adding the user to the sudoers group
 #print_status "Granting sudo privileges to the user..."
 #sudo bash -c "echo '${NAME} ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers"
 
-#print_status "User ${NAME} created successfully with sudo privileges."
 
-# creating a home directory for the new user
-print_status "Creating a Home Directory For New User"
-cd 
-sudo mkdir /home/${NAME}
-
-# Copying configuration files to user's home dir
-sudo cp -r /etc/skel/. /home/${NAME}
-
-# encrypting password
-PASS_ENCRYPTED=$(echo -n "${PASS}" | sha256sum)
-
-# echoing password to make sure it was encrypted successfully
-print_status ${PASS_ENCRYPTED}
-
-INFO="$fullName,$roomNumber,$phoneNumber,$workNumber,$otherInfo"
-HOME_DIR="/home/$NAME"
-concat_passwd "$NAME" "X" "$FIRST_FREE_UID" "$FIRST_FREE_GID" "$INFO" "$HOME_DIR" "$SHELL" 
-concat_prim_group "$NAME" "X" "$FIRST_FREE_GID"
