3a41b73bedce55c5695407a35ff0e2ad0bdae381
AhmedAlyElGhannam <ahmedaly.g112@gmail.com>
2024-12-11 11:52:04 +0200

    everything works but su says authentication failed

--- adduser.sh
+++ adduser.sh
@@ -8,7 +8,25 @@ print_status() {
     echo "$1"
 }
 
-
+# Function to add the user to /etc/shadow
+concat_shadow() {
+    	# username:password:lastchg:min:max:warn:inactive:expire
+    	local USERNAME=$1
+    	local PASSWORD=$2
+    	local LASTCHG=0  	# Last password change (0 means force password change at first login)
+    	local MIN_DAYS=0 	# Minimum days before a password change
+    	local MAX_DAYS=99999 	# Maximum days the password is valid
+    	local WARN_DAYS=7    	# Days to warn user before password expires
+    	local INACTIVE=""     	# Account inactive period (empty means no limit)
+    	local EXPIRE=""       	# Account expiration date (empty means no limit)
+    	local SYMBOL=":"
+	
+	# concatenate relevant fields into one string 
+    	local CONCATENATED="${USERNAME}${SYMBOL}${PASSWORD}${SYMBOL}${LASTCHG}${SYMBOL}${MIN_DAYS}${SYMBOL}${MAX_DAYS}${SYMBOL}${WARN_DAYS}${SYMBOL}${INACTIVE}${SYMBOL}${EXPIRE}"
+	
+	# echo string into shadow
+    	sudo echo "$CONCATENATED" >> /etc/shadow
+}
 
 # concatenate user line to send to /etc/passwd file
 concat_passwd() {
@@ -101,33 +119,14 @@ cd
 sudo mkdir /home/${NAME}
 
 # Copying configuration files to user's home dir
-print_status "Copying Configurations Files for ${NAME} from /etc/skel"
+print_status "Copying Configurations Files for ${NAME} from /etc/skel..."
 sudo cp -r /etc/skel/. /home/${NAME}
 
-# encrypting password
-#PASS_ENCRYPTED=$(echo -n "${PASS}" | sha256sum)
-# Generate the encrypted password using openssl
+# generate the encrypted password using openssl
 PASS_ENCRYPTED=$(openssl passwd -6 "$PASS")
 
-# Function to add the user to /etc/shadow
-concat_shadow() {
-    # username:password:lastchg:min:max:warn:inactive:expire
-    local USERNAME=$1
-    local PASSWORD=$2
-    local LASTCHG=0  # Last password change (0 means force password change at first login)
-    local MIN_DAYS=0 # Minimum days before a password change
-    local MAX_DAYS=99999 # Maximum days the password is valid
-    local WARN_DAYS=7    # Days to warn user before password expires
-    local INACTIVE=      # Account inactive period (empty means no limit)
-    local EXPIRE=        # Account expiration date (empty means no limit)
-    local SYMBOL=":"
-
-    local CONCATENATED="${USERNAME}${SYMBOL}${PASSWORD}${SYMBOL}${LASTCHG}${SYMBOL}${MIN_DAYS}${SYMBOL}${MAX_DAYS}${SYMBOL}${WARN_DAYS}${SYMBOL}${INACTIVE}${SYMBOL}${EXPIRE}"
-
-   sudo echo "$CONCATENATED" >> /etc/shadow
-}
-
-# Add the user to /etc/shadow
+# Add the user's password to /etc/shadow
+print_status "Adding User Password to /etc/shadow..."
 concat_shadow "$NAME" "$PASS_ENCRYPTED"
 
 # appending extra info into one string
@@ -139,7 +138,6 @@ concat_passwd "$NAME" "x" "$FIRST_FREE_UID" "$FIRST_FREE_GID" "$INFO" "$HOME_DIR
 # concatenating all fields required for /etc/group
 concat_prim_group "$NAME" "x" "$FIRST_FREE_GID"
 
-
 # echoing string into passwd
 print_status "Adding ${NAME} to /etc/passwd..."
 echo ${concat_passwd} >> /etc/passwd
@@ -148,6 +146,8 @@ echo ${concat_passwd} >> /etc/passwd
 print_status "Adding ${NAME}'s Group..."
 echo ${concat_prim_group} >> /etc/group
 
+# print exit message
+print_status "Done!"
 
 # adding the user to the sudoers group
 #print_status "Granting sudo privileges to the user..."
